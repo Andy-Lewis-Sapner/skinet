3746c3505d0815413d627ffe59348d7d8583b40c
Andy-Lewis-Sapner <155977415+Andy-Lewis-Sapner@users.noreply.github.com>
2025-07-08 11:34:57 +0300

    End of Section 14

---
+++ API/Controllers/AccountController.cs
@@ -0,0 +1,79 @@
+using API.DTOs;
+using API.Extensions;
+using Core.Entities;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers;
+
+public class AccountController(SignInManager<AppUser> signInManager) : BaseApiController
+{
+    [HttpPost("register")]
+    public async Task<ActionResult> Register(RegisterDto registerDto)
+    {
+        AppUser user = new()
+        {
+            FirstName = registerDto.FirstName,
+            LastName = registerDto.LastName,
+            Email = registerDto.Email,
+            UserName = registerDto.Email
+        };
+
+        IdentityResult result = await signInManager.UserManager.CreateAsync(user, registerDto.Password);
+
+        if (!result.Succeeded)
+        {
+            foreach (IdentityError error in result.Errors)
+            {
+                ModelState.AddModelError(error.Code, error.Description);
+            }
+
+            return ValidationProblem();
+        }
+
+        return Ok();
+    }
+
+    [Authorize]
+    [HttpPost("logout")]
+    public async Task<ActionResult> Logout()
+    {
+        await signInManager.SignOutAsync();
+        return NoContent();
+    }
+
+    [HttpGet("user-info")]
+    public async Task<ActionResult> GetUserInfo()
+    {
+        if (User.Identity?.IsAuthenticated == false) return NoContent();
+        AppUser? user = await signInManager.UserManager.GetUserByEmailWithAddress(User);
+        return Ok(new
+        {
+            user.FirstName,
+            user.LastName,
+            user.Email,
+            Address = user.Address?.ToDto()
+        });
+    }
+
+    [HttpGet]
+    public ActionResult GetAuthState()
+    {
+        return Ok(new { isAuthenticated = User.Identity?.IsAuthenticated ?? false });
+    }
+
+    [Authorize]
+    [HttpPost("address")]
+    public async Task<ActionResult<Address>> CreateOrUpdateAddress(AddressDto addressDto)
+    {
+        AppUser user = await signInManager.UserManager.GetUserByEmailWithAddress(User);
+
+        if (user.Address == null) user.Address = addressDto.ToEntity();
+        else user.Address.UpdateFromDto(addressDto);
+
+        IdentityResult result = await signInManager.UserManager.UpdateAsync(user);
+        if (!result.Succeeded) return BadRequest("Problem updating user address");
+        return Ok(user.Address.ToDto());
+    } 
+}
--- API/Program.cs
+++ API/Program.cs
@@ -1,4 +1,5 @@
 using API.Middleware;
+using Core.Entities;
 using Core.Interfaces;
 using Infrastructure.Data;
 using Infrastructure.Services;
@@ -25,6 +26,9 @@ builder.Services.AddSingleton<IConnectionMultiplexer>(config =>
     return ConnectionMultiplexer.Connect(configuration);
 });
 builder.Services.AddSingleton<ICartService, CartService>();
+builder.Services.AddAuthorization();
+builder.Services.AddIdentityApiEndpoints<AppUser>()
+    .AddEntityFrameworkStores<StoreContext>();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 // builder.Services.AddOpenApi();
 
@@ -41,10 +45,11 @@ app.UseHttpsRedirection();
 // app.UseAuthorization();
 app.UseMiddleware<ExceptionMiddleware>();
 
-app.UseCors(x => x.AllowAnyHeader().AllowAnyMethod()
+app.UseCors(x => x.AllowAnyHeader().AllowAnyMethod().AllowCredentials()
     .WithOrigins("http://localhost:4200", "https://localhost:4200"));
 
 app.MapControllers();
+app.MapGroup("api").MapIdentityApi<AppUser>();
 
 try
 {
